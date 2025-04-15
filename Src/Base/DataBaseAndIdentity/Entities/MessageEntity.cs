ba7588e2449ff82bb0745c1486cc585403d81a44
Vu Vu Developer <vuvo070403@gmail.com>
2025-04-15 11:17:29 +0700

    [VuVV2]-Fixed Entity Config

--- Src/Base/DataBaseAndIdentity/Entities/MessageEntity.cs
+++ Src/Base/DataBaseAndIdentity/Entities/MessageEntity.cs
@@ -7,7 +7,6 @@ public sealed class MessageEntity : BaseEntity<Guid>
     public string Content { get; set; }
     public string MessageType { get; set; }
     public string SentAt { get; set; }
-
     public Guid HistoryId { get; set; }
 
     #region Navigations
