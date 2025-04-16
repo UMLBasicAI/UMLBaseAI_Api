e94db978fad6b2bcbadd95cf37b862873598f22f
CheeseMink <mjnhtran2004@gmail.com>
2025-04-16 15:49:56 +0700

    [MinhTC] Add feature get list of messages of history by historyId

---
+++ Src/Core/AI/GetPromptHistories/Models/AppRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using FCommon.FeatureService;
+
+namespace GetSinglePromptHistory.Models;
+
+public sealed class AppRequestModel : IServiceRequest<AppResponseModel>
+{
+    public int Page { get; set; }
+    public int Size { get; set; }
+    public string HistoryId { get; set; }
+}
